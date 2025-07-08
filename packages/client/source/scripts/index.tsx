cddfd0e258e2360280bf44b848910715d84e2541
peaceful star <peastar@icloud.com>
2025-07-08 20:35:17 +0900

    Update

--- packages/client/source/scripts/index.tsx
+++ packages/client/source/scripts/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import { Authenticator } from '@aws-amplify/ui-react';
+// import { Authenticator } from '@aws-amplify/ui-react';
 // import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../../../../amplify_outputs.json";
@@ -31,9 +31,9 @@ const client = new ApolloClient({
 root.render(
   <ApolloProvider client={client}>
     <BrowserRouter>
-      <Authenticator>
+      {/*<Authenticator>*/}
         <App />
-      </Authenticator>
+      {/*</Authenticator>*/}
     </BrowserRouter>
   </ApolloProvider>,
 );
